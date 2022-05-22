76346a159fa0cdef74c48e954c4736db76f5a091
sagor devnath <ssagor64@gmail.com>
2022-05-23 01:40:19 +0600

    user api created

--- index.js
+++ index.js
@@ -14,17 +14,55 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 
 
 async function run() {
     try {
       await client.connect();
       const productCollection = client.db('computer-manufacturers').collection('products');
+      const userCollection = client.db('computer-manufacturers').collection('users');      
+      const orderCollection = client.db('computer-manufacturers').collection('orders');
 
       app.get('/product', async(req, res) => {
           const products = await productCollection.find().toArray();
           res.send(products);
-      })
+      });
+
+      app.get('/product/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const product = await productCollection.findOne(query);
+        res.send(product);
+      });
+
+    
+
+      app.put('/user/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: user,
+        };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+        const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        res.send({ result, token});
+      });
      
     } finally {
       
